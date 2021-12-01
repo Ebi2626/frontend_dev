d7b0ae021db00c3763717d468bf216dabf67782d
Edwin <edwin.harmata@gmail.com>
2021-12-01 20:47:33 +0100

    Sparate front-page in wordpress hierarchy

--- single.php
+++ single.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<main id="content" role="main">
+<main class="container" id="content" role="main">
 
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <?php get_template_part( 'entry' ); ?>
